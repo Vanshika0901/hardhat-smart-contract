8e7e8f4fa30298f82bf3dc9e959454ef62199b45
alithegilfoyle <alimurtaza-assistant@outlook.com>
2022-08-26 16:35:27 +0500

    fix issues in deploy scripts

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,5 +1,5 @@
 const { getNamedAccounts, deployments, network } = require("hardhat");
-const { developmentChains } = require("../helper-hardhat-config");
+const { deploymentChains } = require("../helper-hardhat-config");
 
 const BASE_FEE = ethers.utils.parseEther("0.25"); // it is a premium, it costs 0.25 link per request
 const GAS_PRICE_LINK = 1e9;
@@ -11,10 +11,10 @@ module.exports = async function (hre) {
   const chainId = network.config.chainId;
   const args = [BASE_FEE, GAS_PRICE_LINK];
 
-  if (developmentChains.includes(network.name)) {
+  if (deploymentChains.includes(network.name)) {
     log("Local network detected! Deploying mocks...");
     //deploy our mock vrfCoordinatorV2
-    await deploy("VRFCoordinatorV2", {
+    await deploy("VRFCoordinatorV2Mock", {
       from: deployer,
       log: true,
       args: args,
